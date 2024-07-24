41bffc483a00b4717b891c6af609d449896a6632
ramses.ramirez <ramses.ramirez@exodus.com.mx>
2024-07-23 19:07:44 -0600

    feat: Add SendMail function to handle email sending logic

--- api/handlers/mailHandler.go
+++ api/handlers/mailHandler.go
@@ -1,11 +1,35 @@
 package handlers
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"github.com/HouseCham/mailService/internal/mail"
+	"github.com/HouseCham/mailService/internal/model"
+	"github.com/gofiber/fiber/v3"
+)
 
 // Handler function for sending mail
 func SendMail(c fiber.Ctx) error {
-	// Send mail logic here
+	var request model.UserData
+	var data string
+	var statusCode int
+
+	// Parse the request body from POST request
+	c.Bind().JSON(&request)
+	
+	// Send the mail
+	sendMailError := mail.SendMail(request)
+	if sendMailError != nil {
+		data = sendMailError.Error()
+		statusCode = fiber.StatusInternalServerError
+	} else {
+		data = "Mail sent successfully"
+		statusCode = fiber.StatusOK
+	}
+	
+	// Return the response
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
-		"message": "Mail sent successfully",
+		"HasError":   sendMailError != nil,
+		"Error":      sendMailError,
+		"Data":       data,
+		"StatusCode": statusCode,
 	})
 }
